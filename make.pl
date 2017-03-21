b886d4a37ee56f4fdf0d84cbddbcdde23de10525
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-03-22 02:17:33 +0500

    fix for paths issue

--- make.pl
+++ make.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/env perl
 use v5.10; use strict; use warnings; use autodie qw(:all);
 die 'unexpected arguments count' if scalar(@ARGV) != 1;
-use Env qw<PWD HOME>;
+use Env qw<HOME>;
+use Cwd qw(abs_path);
 use IPC::System::Simple qw<runx>;
+
+my $__dirname = abs_path './';
 
 if ($ARGV[0] eq 'create-symlink') {
 
   chdir "$HOME/.config/";
-  runx 'ln', '-s', '--', "$PWD/", 'nvim';
+  runx 'ln', '-s', '--', "$__dirname/", 'nvim';
 
 } elsif ($ARGV[0] eq 'clean') {
 
