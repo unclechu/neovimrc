1dd32da673d0a9ff3356b2d8d53569af4963852c
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-09-30 02:11:27 +0300

    Nix: Update default nixpkgs pick

--- nix/default-nixpkgs-pick.nix
+++ nix/default-nixpkgs-pick.nix
@@ -1,12 +1,12 @@
 let
   nixpkgs-src =
     let
-      # ref "release-20.09", 14 September 2020
-      commit = "5f5d8947d2583eeeba7b9e350506e15d6632af1e";
+      # ref "release-20.09", 30 September 2020
+      commit = "663ff8739c59c492d215f3a6e65968a16a7df8a8";
     in
       fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
-        sha256 = "15ra369rhdxd747jm0ylr8sd6vpnhsqdbl7h9hr1fhnv33p49ngj";
+        sha256 = "0g2nn388cvc21ihk3w3pzglvn6hi57f5pzgiqq3fx5sl1fkvxw05";
       };
 in
 import nixpkgs-src {} // { inherit nixpkgs-src; }
