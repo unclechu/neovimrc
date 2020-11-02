0212fda3509592d4ee10b166c13eff3070d68da2
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2020-11-02 23:11:16 +0200

    Nix: Update nix/default-nixpkgs-pick.nix

--- nix/default-nixpkgs-pick.nix
+++ nix/default-nixpkgs-pick.nix
@@ -1,12 +1,12 @@
 let
   nixpkgs-src =
     let
-      # ref “release-20.09”, 6 October 2020
-      commit = "e350a3327e5604a81752581fb81c91475bf4c29a";
+      # ref “release-20.09”, 2 November 2020
+      commit = "faee35ae82cc54bff6e5235895d4968f0de8d39a";
     in
       fetchTarball {
         url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
-        sha256 = "0jlfnnpsic9f92k8h5bq8ykk1g3shgny6f5d6q7xrs670gp5ni3k";
+        sha256 = "0jgp6czvqxh30hnq1a3l24mahcmqh6ik729jw1vq13pllhbb2p4b";
       };
 in
 import nixpkgs-src {} // { inherit nixpkgs-src; }
