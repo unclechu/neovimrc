1dd9eae1507642c33195d7f955da45e777e3d8ec
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2024-02-14 21:08:01 +0200

    Nix: Move TreeSitter plugin setup into own module

--- nix/plugins.nix
+++ nix/plugins.nix
@@ -47,54 +47,7 @@ let
     #   "bullets.vim"
     #   (nix-gitignore.gitignoreRecursiveSource [ ../bullets.vim/.gitignore ] ../bullets.vim);
 
-    "nvim-treesitter" = vimPlugins.nvim-treesitter.withPlugins (tsPlugins: [
-      tsPlugins.haskell
-      tsPlugins.purescript
-
-      tsPlugins.lua
-      tsPlugins.perl
-      tsPlugins.python
-      tsPlugins.ruby
-
-      tsPlugins.java
-      tsPlugins.clojure
-      tsPlugins.kotlin
-
-      tsPlugins.yaml
-      tsPlugins.json
-      tsPlugins.jq
-      tsPlugins.xml
-
-      tsPlugins.bash
-
-      # Not really useful, only “diff” command and commit hashes are highlighted.
-      # The most important stuff to have highlighted is additions and deletions.
-      # By not adding it here making Neovim to use the non-treesitter syntax.
-      # tsPlugins.diff
-
-      tsPlugins.make
-      tsPlugins.cmake
-
-      tsPlugins.c
-      tsPlugins.cpp
-
-      tsPlugins.html
-      tsPlugins.css
-      tsPlugins.javascript
-      tsPlugins.typescript
-      tsPlugins.tsx
-
-      tsPlugins.supercollider
-
-      # Etc
-      tsPlugins.vim
-      tsPlugins.markdown
-      tsPlugins.nix
-      tsPlugins.sql
-      tsPlugins.glsl
-      tsPlugins.dot
-      tsPlugins.dockerfile
-    ]);
+    nvim-treesitter = callPackage plugins/treesitter.nix {};
   };
 
   mkGhPlugin = plugin:
---
+++ nix/plugins/treesitter.nix
@@ -0,0 +1,64 @@
+# Author: Viacheslav Lotsmanov
+# License: MIT https://raw.githubusercontent.com/unclechu/neovimrc/master/LICENSE
+
+# TreeSitter plugin with added syntax/filetype plugins for it.
+#
+# See https://github.com/nvim-treesitter/nvim-treesitter
+# See https://tree-sitter.github.io/tree-sitter/
+
+# This module is intended to be called with ‘nixpkgs.callPackage’
+{ vimPlugins
+}:
+
+let
+  treesitter = vimPlugins.nvim-treesitter.withPlugins (tsPlugins: [
+    tsPlugins.haskell
+    tsPlugins.purescript
+
+    tsPlugins.lua
+    tsPlugins.perl
+    tsPlugins.python
+    tsPlugins.ruby
+
+    tsPlugins.java
+    tsPlugins.clojure
+    tsPlugins.kotlin
+
+    tsPlugins.yaml
+    tsPlugins.json
+    tsPlugins.jq
+    tsPlugins.xml
+
+    tsPlugins.bash
+
+    # Not really useful, only “diff” command and commit hashes are highlighted.
+    # The most important stuff to have highlighted is additions and deletions.
+    # By not adding it here making Neovim to use the non-treesitter syntax.
+    # tsPlugins.diff
+
+    tsPlugins.make
+    tsPlugins.cmake
+
+    tsPlugins.c
+    tsPlugins.cpp
+
+    tsPlugins.html
+    tsPlugins.css
+    tsPlugins.javascript
+    tsPlugins.typescript
+    tsPlugins.tsx
+
+    tsPlugins.supercollider
+
+    # Etc
+    tsPlugins.vim
+    tsPlugins.markdown
+    tsPlugins.nix
+    tsPlugins.sql
+    tsPlugins.glsl
+    tsPlugins.dot
+    tsPlugins.dockerfile
+  ]);
+in
+
+treesitter
